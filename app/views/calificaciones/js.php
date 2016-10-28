e14b0f46619af29a3083c7d06344a939d4b32049
Ender Galban <endergalban@gmail.com>
2016-10-27 22:36:36 -0400

    Update js.php
    Corrección del problema de la carga de registros

--- app/views/calificaciones/js.php
+++ app/views/calificaciones/js.php
@@ -1,34 +1,35 @@
 <script>
-		$(document).ready(function() {
-			var table = $('#datatableServerSide').DataTable({
-				"processing": true, //Feature control the processing indicator.
-				"serverSide": true, //Feature control DataTables' server-side processing mode.
-				"order": [], //Initial no order.
-				"language":{
-						"url": "//cdn.datatables.net/plug-ins/1.10.12/i18n/Spanish.json"
-				},
-				// Load data for the table's content from an Ajax source
-				"ajax": {
-					"dataType": "json",
-					"url": "<?php echo site_url('user/ajax_calificaciones')?>",
-					"type": "POST"
-				},
-				aoColumns:[
-						{"sClass":""},
-						{"sClass":""},
-						{"sClass":"text-center"},
-						{"sClass":"text-center"},
-						{"sClass":"text-center"},
-						{"sClass":"text-center"},
-						{"sClass":"text-center"},
-				],
-				//Set column definition initialisation properties.
-				"columnDefs": [
-				{
-					"targets": [ 6], //first column / numbering column
-					"orderable": false, //set not orderable
-				},
-				],
-			});
+	$(document).ready(function() {
+		var csrf_test_name_notas = '<?php echo $this->security->get_csrf_hash(); ?>';
+		var table = $('#datatableServerSide').DataTable({
+			"processing": true, //Feature control the processing indicator.
+			"serverSide": true, //Feature control DataTables' server-side processing mode.
+			"order": [], //Initial no order.
+			"language":{
+					"url": "//cdn.datatables.net/plug-ins/1.10.12/i18n/Spanish.json"
+			},
+			// Load data for the table's content from an Ajax source
+			"ajax": {
+				"dataType": "json",
+				"url": "<?php echo site_url('user/ajax_estudiantes')?>",
+				"type": "POST",
+				"data": {"csrf_test_name_notas": csrf_test_name_notas }
+			},
+			aoColumns:[
+					{"sClass":""},
+					{"sClass":""},
+					{"sClass":"text-center"},
+					{"sClass":"text-center"},
+					{"sClass":"text-center"},
+
+			],
+			//Set column definition initialisation properties.
+			"columnDefs": [
+			{
+				"targets": [ 4], //first column / numbering column
+				"orderable": false, //set not orderable
+			},
+			],
 		});
-	</script>
+	});
+</script>
